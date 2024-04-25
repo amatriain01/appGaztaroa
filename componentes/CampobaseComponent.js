df304b3309eb48ae46b7075ebab48fa78bf1f89f
Adrian Amatriain <amatriain.133735@e.unavarra.es>
2024-04-25 19:58:15 +0200

    Botones o Iconos

--- componentes/CampobaseComponent.js
+++ componentes/CampobaseComponent.js
@@ -4,16 +4,36 @@ import Calendario from './CalendarioComponent';
 import DetalleExcursion from './DetalleExcursionComponent';
 import Contacto from './ContactoComponent';
 import QuienesSomos from './QuienesSomosComponent';
-import { Platform, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { Platform, View, StyleSheet, Image, Text } from 'react-native';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import Home from './HomeComponent';
+import { Icon } from '@rneui/themed';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
-function CalendarioNavegador() {
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
+        <View style={styles.drawerHeader}>
+          <View style={{ flex: 1 }}>
+            <Image source={require('./imagenes/logo.png')} style={styles.drawerImage} />
+          </View>
+          <View style={{ flex: 2 }}>
+            <Text style={styles.drawerHeaderText}> Gaztaroa</Text>
+          </View>
+        </View>
+        <DrawerItemList {...props} />
+      </SafeAreaView>
+    </DrawerContentScrollView>
+  );
+}
+
+function CalendarioNavegador({ navigation }) {
   return (
     <Stack.Navigator
       initialRouteName="CalendarioNavegador"
@@ -30,6 +50,7 @@ function CalendarioNavegador() {
         component={Calendario}
         options={{
           title: 'Calendario',
+          headerLeft: () => (<Icon name="menu" size={28} color='white' onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())} />)
         }}
       />
       <Stack.Screen
@@ -43,7 +64,7 @@ function CalendarioNavegador() {
   );
 }
 
-function HomeNavegador() {
+function HomeNavegador({ navigation }) {
   return (
     <Stack.Navigator
       initialRouteName="HomeNavegador"
@@ -52,7 +73,8 @@ function HomeNavegador() {
         headerTintColor: '#fff',
         headerStyle: { backgroundColor: '#015afc' },
         headerTitleStyle: { color: '#fff' },
-        headerTitleAlign: 'center'
+        headerTitleAlign: 'center',
+        headerLeft: () => (<Icon name="menu" size={28} color='white' onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())} />)
       }}
     >
       <Stack.Screen
@@ -66,7 +88,7 @@ function HomeNavegador() {
   );
 }
 
-function ContactoNavegador() {
+function ContactoNavegador({ navigation }) {
   return (
     <Stack.Navigator
       initialRouteName="ContactoNavegador"
@@ -75,7 +97,8 @@ function ContactoNavegador() {
         headerTintColor: '#fff',
         headerStyle: { backgroundColor: '#015afc' },
         headerTitleStyle: { color: '#fff' },
-        headerTitleAlign: 'center'
+        headerTitleAlign: 'center',
+        headerLeft: () => (<Icon name="menu" size={28} color='white' onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())} />)
       }}
     >
       <Stack.Screen
@@ -89,7 +112,7 @@ function ContactoNavegador() {
   );
 }
 
-function QuienesSomosNavegador() {
+function QuienesSomosNavegador({ navigation }) {
   return (
     <Stack.Navigator
       initialRouteName="QuienesSomosNavegador"
@@ -98,7 +121,8 @@ function QuienesSomosNavegador() {
         headerTintColor: '#fff',
         headerStyle: { backgroundColor: '#015afc' },
         headerTitleStyle: { color: '#fff' },
-        headerTitleAlign: 'center'
+        headerTitleAlign: 'center',
+        headerLeft: () => (<Icon name="menu" size={28} color='white' onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())} />)
       }}
     >
       <Stack.Screen
@@ -115,7 +139,8 @@ function QuienesSomosNavegador() {
 function DrawerNavegador() {
   return (
     <Drawer.Navigator
-      initialRouteName=" Drawer"
+      initialRouteName="Campo Base"
+      drawerContent={props => <CustomDrawerContent {...props} />}
       screenOptions={{
         headerShown: false,
         drawerStyle: {
@@ -123,14 +148,57 @@ function DrawerNavegador() {
         },
       }}
     >
-      <Drawer.Screen name="Campo Base" component={HomeNavegador} />
-      <Drawer.Screen name="Quiénes Somos" component={QuienesSomosNavegador} />
-      <Drawer.Screen name="Calendario" component={CalendarioNavegador} />
-      <Drawer.Screen name="Contacto" component={ContactoNavegador} />
+      <Drawer.Screen name="Campo Base" component={HomeNavegador}
+        options={{
+          drawerIcon: ({ tintColor }) => (
+            <Icon
+              name='home'
+              type='font-awesome'
+              size={24}
+              color={tintColor}
+            />
+          )
+        }}
+      />
+      <Drawer.Screen name="Quiénes Somos" component={QuienesSomosNavegador}
+        options={{
+          drawerIcon: ({ tintColor }) => (
+            <Icon
+              name='info-circle'
+              type='font-awesome'
+              size={24}
+              color={tintColor}
+            />
+          )
+        }}
+      />
+      <Drawer.Screen name="Calendario" component={CalendarioNavegador}
+        options={{
+          drawerIcon: ({ tintColor }) => (
+            <Icon
+              name='calendar'
+              type='font-awesome'
+              size={24}
+              color={tintColor}
+            />
+          )
+        }}
+      />
+      <Drawer.Screen name="Contacto" component={ContactoNavegador}
+        options={{
+          drawerIcon: ({ tintColor }) => (
+            <Icon
+              name='address-card'
+              type='font-awesome'
+              size={24}
+              color={tintColor}
+            />
+          )
+        }}
+      />
     </Drawer.Navigator>
   );
 }
-
 
 class Campobase extends Component {
   render() {
@@ -143,5 +211,29 @@ class Campobase extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  drawerHeader: {
+    backgroundColor: '#015afc',
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row'
+  },
+  drawerHeaderText: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  drawerImage: {
+    margin: 10,
+    width: 80,
+    height: 60
+  }
+});
 
 export default Campobase;
--- componentes/DetalleExcursionComponent.js
+++ componentes/DetalleExcursionComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Card } from '@rneui/themed';
+import { Text, View, ScrollView, FlatList } from 'react-native';
+import { Card, Icon } from '@rneui/themed';
 import { EXCURSIONES } from '../comun/excursiones';
+import { COMENTARIOS } from '../comun/comentarios';
 
 function RenderExcursion(props) {
 
@@ -17,6 +18,53 @@ function RenderExcursion(props) {
                 <Text style={{ margin: 20 }}>
                     {excursion.descripcion}
                 </Text>
+                <Icon
+                    raised
+                    reverse
+                    name={props.favorita ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    onPress={() => props.favorita ? console.log('La excursión ya se encuentra entre las favoritas') : props.onPress()}
+                />
+            </Card>
+        );
+    }
+    else {
+        return (<View></View>);
+    }
+}
+
+function FormatDate(dateString) {
+    var date = new Date(dateString.replace(/\s/g, ''));
+    var year = date.getFullYear();
+    var month = ('0' + (date.getMonth() + 1)).slice(-2);
+    var day = ('0' + date.getDate()).slice(-2);
+    var hours = ('0' + date.getHours()).slice(-2);
+    var minutes = ('0' + date.getMinutes()).slice(-2);
+    var seconds = ('0' + date.getSeconds()).slice(-2);
+    return day + '/' + month + '/' + year + ', ' + hours + ':' + minutes + ':' + seconds;
+}
+
+function RenderComentario(props) {
+
+    const comentarios = props.comentarios;
+    if (comentarios != null) {
+        return (
+            <Card>
+                <Card.Title>Comentarios</Card.Title>
+                <FlatList
+                    scrollEnabled={false}
+                    data={comentarios}
+                    renderItem={({ item }) => (
+                        <View key={item.id}>
+                            <Card.Divider />
+                            <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{item.autor}:</Text>
+                            <Text style={{ fontSize: 14, marginTop: 5, marginBottom: 5 }}>{item.comentario}</Text>
+                            <Text style={{ fontSize: 12, marginBottom: 5, fontWeight: 'bold', color: (item.valoracion) > 2 ? 'green' : 'red' }}>{item.valoracion} / 5</Text>
+                            <Text style={{ fontSize: 10, marginBottom: 20 }}>{FormatDate(item.dia)}</Text>
+                        </View>
+                    )}
+                />
             </Card>
         );
     }
@@ -29,14 +77,33 @@ class DetalleExcursion extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            excursiones: EXCURSIONES
+            excursiones: EXCURSIONES,
+            comentarios: COMENTARIOS,
+            favoritos: []
         };
     }
 
+    marcarFavorito(excursionId) {
+        this.setState({ favoritos: this.state.favoritos.concat(excursionId) });
+    }
+
+
     render() {
         const { excursionId } = this.props.route.params;
-        return (<RenderExcursion excursion={this.state.excursiones[+excursionId]} />);
+        return (
+            <ScrollView>
+                <RenderExcursion
+                    excursion={this.state.excursiones[+excursionId]}
+                    favorita={this.state.favoritos.some(el => el === excursionId)}
+                    onPress={() => this.marcarFavorito(excursionId)}
+                />
+                <RenderComentario
+                    comentarios={this.state.comentarios.filter((comentario) => comentario.excursionId === excursionId)}
+                />
+            </ScrollView>
+        );
     }
+
 }
 
 export default DetalleExcursion;
